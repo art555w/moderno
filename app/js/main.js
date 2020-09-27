89f7d307f11c02e5f7343bf60c694caa2f1068bc
Your Nameart555w <art555m@gmail.com>
2020-09-27 11:26:58 +0500

    added product-details.html

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,15 @@ $(function () {
 		$('.header__inner').toggleClass('active')
 	})
 
+	$('.details__tabs .tab').on('click', function (event) {
+		var id = $(this).attr('data-id');
+		$('.details__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.details__tabs .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#' + id).addClass('active-tab').fadeIn();
+		return false;
+	});
+
 	var mixer = mixitup('.products__inner-body');
 
 
